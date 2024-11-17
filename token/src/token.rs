6f1e1083b1212be947e28a593a926ae8ad02a43b
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2024-11-17 15:26:41 +0000

    Add token column start and end pointers

--- token/src/token.rs
+++ token/src/token.rs
@@ -5,14 +5,24 @@ pub struct Token<'source> {
     token_type: TokenType,
     literal: &'source str,
     line_number: usize,
+    column_start: usize,
+    column_end: usize,
 }
 
 impl<'source> Token<'source> {
-    pub fn new(token_type: TokenType, literal: &'source str, line_number: usize) -> Self {
+    pub fn new(
+        token_type: TokenType,
+        literal: &'source str,
+        line_number: usize,
+        column_start: usize,
+        column_end: usize,
+    ) -> Self {
         Self {
             token_type,
             literal,
             line_number,
+            column_start,
+            column_end,
         }
     }
 }
@@ -22,12 +32,24 @@ mod token_tests {
     use super::*;
 
     #[test]
+    fn create_column_span_from_token_position() {
+        let source = "{ \"is_ok\": false }";
+
+        let start = 11;
+        let current = 16;
+
+        let t = Token::new(TokenType::False, &source[start..current], 1, start, current);
+
+        assert_eq!(11..16, t.column_start..t.column_end);
+    }
+
+    #[test]
     fn retrieve_literal_from_token() {
         let source = "[]";
 
         assert_eq!(
             "[",
-            Token::new(TokenType::LeftBracket, &source[0..1], 1).literal
+            Token::new(TokenType::LeftBracket, &source[0..1], 1, 0, 1).literal
         );
     }
 
@@ -36,16 +58,22 @@ mod token_tests {
         let source = String::from("}");
 
         assert_eq!(
-            Token::new(TokenType::RightBrace, "}", 1),
-            Token::new(TokenType::RightBrace, &source[0..1], 1)
+            Token::new(TokenType::RightBrace, "}", 1, 0, 1),
+            Token::new(TokenType::RightBrace, &source[0..1], 1, 0, 1)
+        );
+
+        let source = String::from("\"hello\"");
+        assert_eq!(
+            "\"hello\"",
+            Token::new(TokenType::RightBrace, &source[0..7], 1, 0, 7).literal
         );
     }
 
     #[test]
     fn create_new_token() {
         assert_eq!(
-            Token::new(TokenType::String, "\"hello\"", 1),
-            Token::new(TokenType::String, "\"hello\"", 1)
+            Token::new(TokenType::String, "\"hello\"", 1, 0, 7),
+            Token::new(TokenType::String, "\"hello\"", 1, 0, 7)
         );
     }
 }
