b77701dff997ea9d98faffefcfc7a3ddcdf2e0d3
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2024-11-19 14:31:52 +0000

    Created parser error basic

---
+++ parser/src/parser_error.rs
@@ -0,0 +1,26 @@
+use std::fmt::Display;
+
+#[derive(Debug)]
+pub enum ParserError {
+    UnexpectedToken,
+}
+
+impl std::error::Error for ParserError {}
+
+impl Display for ParserError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ParserError::UnexpectedToken => write!(f, "Unexpected Token"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod parser_error_tests {
+    use crate::parser_error::ParserError;
+
+    #[test]
+    fn unexpected_token_message() {
+        assert_eq!("Unexpected Token", ParserError::UnexpectedToken.to_string());
+    }
+}
