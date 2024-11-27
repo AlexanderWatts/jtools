638b8000254e5b3619c7ba759296e5fd2ab71257
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2024-11-27 14:24:31 +0000

    Create new scanner

--- scanner/src/scanner.rs
+++ scanner/src/scanner.rs
@@ -1,3 +1,30 @@
+use std::{iter::Peekable, str::CharIndices};
+
+#[derive(Debug)]
+pub struct Scanner<'source> {
+    source: &'source str,
+    chars: Peekable<CharIndices<'source>>,
+    start: usize,
+    current: usize,
+    line: usize,
+    column_start: usize,
+    column_end: usize,
+}
+
+impl<'source> Scanner<'source> {
+    pub fn new(source: &'source str) -> Self {
+        Self {
+            source,
+            chars: source.char_indices().peekable(),
+            start: 0,
+            current: 0,
+            line: 1,
+            column_start: 1,
+            column_end: 1,
+        }
+    }
+}
+
 #[cfg(test)]
 mod scanner_tests {
     use super::*;
