8d9369bf36599f2c8cda8e265957d4f1859035df
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2024-12-10 16:05:20 +0000

    Update error display test and docs

--- error_display/src/error_display.rs
+++ error_display/src/error_display.rs
@@ -3,6 +3,41 @@ use std::ops::ControlFlow;
 #[derive(Debug, PartialEq)]
 pub struct ErrorDisplay;
 
+/// Create an error to be displayed to the user
+///
+/// ## Description
+///
+/// Error display is used in the scanner and parser to generate a display message for an error
+/// creating a preview of the source input around the start and end of an error where it also
+/// marks it starting position.
+///
+/// The algorithm implemented in `preview` iterates from the start and end of an error within an
+/// input to some distance/limit `n`. This had to be done as indexing into an input becomes more
+/// difficult with grapheme clusters where you cannot know if an index is pointing to some code
+/// point in the middle of a grapheme, which causes an error.
+///
+/// ## Examples
+///
+/// ```
+/// use error_display::error_display::ErrorDisplay;
+///
+/// let source = "{ \"error\": bad }";
+/// let error_display = ErrorDisplay;
+///
+/// assert_eq!(
+///     "\n  |\n  |\n1 | { \"error\": bad }\n  |            ^\n  |",
+///     error_display.preview(source, 11, 13, 1)
+/// );
+/// ```
+///
+/// ### Output:
+/// ```text
+///   |
+///   |
+/// 1 | { "error": bad }
+///   |            ^
+///   |
+/// ```
 impl ErrorDisplay {
     pub fn preview(&self, source: &str, start: usize, current: usize, line: usize) -> String {
         let limit = 15;
@@ -52,5 +87,13 @@ mod preview_tests {
     use super::*;
 
     #[test]
-    fn display() {}
+    fn display_error() {
+        let source = "{ \"error\": bad }";
+        let ed = ErrorDisplay;
+
+        assert_eq!(
+            "\n  |\n  |\n1 | { \"error\": bad }\n  |            ^\n  |",
+            ed.preview(source, 11, 13, 1)
+        );
+    }
 }
