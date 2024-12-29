6d105bf8aea9ad473bcddde9228f8084f4ed7a45
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2024-12-29 14:29:58 +0000

    Update bench tests to only parse given inputs and update readme

--- tests/tests/bench.rs
+++ tests/tests/bench.rs
@@ -1,6 +1,5 @@
 use core::str;
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
-use format::formatter::Formatter;
 use parser::parser::Parser;
 use scanner::scanner::Scanner;
 use std::fs::{self};
@@ -10,22 +9,19 @@ fn scan_to_format(source: &str) {
 
     let tokens = match scanner.scan() {
         Ok(tokens) => tokens,
-
         Err(error) => {
             return eprintln!("{}", error);
         }
     };
 
     let parser = Parser::new(source, tokens);
-    let ast = match parser.parse() {
+
+    let _ = match parser.parse() {
         Ok(ast) => ast,
         Err(error) => {
             return eprintln!("{}", error);
         }
     };
-
-    let formatter = Formatter::default();
-    let _ = formatter.format(&ast);
 }
 
 fn read(path: &str) -> String {
@@ -34,8 +30,29 @@ fn read(path: &str) -> String {
     str::from_utf8(&bytes).unwrap().to_string()
 }
 
-fn multi_source_size(c: &mut Criterion) {
-    let mut group = c.benchmark_group("json");
+fn minified_json(c: &mut Criterion) {
+    let mut group = c.benchmark_group("minified_json");
+
+    let inputs = vec![
+        ("64KB", read("json/64KB-min.json")),
+        ("128KB", read("json/128KB-min.json")),
+        ("256KB", read("json/256KB-min.json")),
+        ("512KB", read("json/512KB-min.json")),
+        ("1MB", read("json/1MB-min.json")),
+        ("5MB", read("json/5MB-min.json")),
+    ];
+
+    for (name, input) in inputs.into_iter() {
+        group.bench_with_input(BenchmarkId::new("size", name), &input, |b, s| {
+            b.iter(|| black_box(scan_to_format(&s)));
+        });
+    }
+
+    group.finish();
+}
+
+fn formatted_json(c: &mut Criterion) {
+    let mut group = c.benchmark_group("formatted_json");
 
     let inputs = vec![
         ("64KB", read("json/64KB.json")),
@@ -44,11 +61,10 @@ fn multi_source_size(c: &mut Criterion) {
         ("512KB", read("json/512KB.json")),
         ("1MB", read("json/1MB.json")),
         ("5MB", read("json/5MB.json")),
-        ("92MB", read("json/92MB.json")),
     ];
 
     for (name, input) in inputs.into_iter() {
-        group.bench_with_input(BenchmarkId::new("file", name), &input, |b, s| {
+        group.bench_with_input(BenchmarkId::new("size", name), &input, |b, s| {
             b.iter(|| black_box(scan_to_format(&s)));
         });
     }
@@ -56,5 +72,5 @@ fn multi_source_size(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, multi_source_size);
+criterion_group!(benches, minified_json, formatted_json);
 criterion_main!(benches);
