82b5b6384b09cca4f7d3a167374b09d98dbabaa7
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2025-01-07 23:17:56 +0000

    Put scanner error back to how it was

--- scanner/src/scanner_error.rs
+++ scanner/src/scanner_error.rs
@@ -1,52 +1,19 @@
 use std::{error::Error, fmt::Display};
 
-use error_display::error_display::ErrorDisplay;
-
 #[derive(Debug, PartialEq)]
 pub enum ScannerError {
-    //Test { preview: Preview },
-    EmptySource {
-        error: String,
-    },
-    UnknownCharacter {
-        error: String,
-    },
-    UnknownLiteral {
-        error: String,
-    },
-    UnterminatedString {
-        error: String,
-    },
-    UnterminatedFractionalNumber {
-        error: String,
-    },
-    LeadingZeros {
-        error: String,
-    },
-    InvalidExponent {
-        error: String,
-    },
-    InvalidNumber {
-        error: String,
-    },
-    InvalidEscapeSequence {
-        error: String,
-    },
-    InvalidUnicodeSequence {
-        source: String,
-        start: usize,
-        column_start: usize,
-        line: usize,
-    },
+    EmptySource { error: String },
+    UnknownCharacter { error: String },
+    UnknownLiteral { error: String },
+    UnterminatedString { error: String },
+    UnterminatedFractionalNumber { error: String },
+    LeadingZeros { error: String },
+    InvalidExponent { error: String },
+    InvalidNumber { error: String },
+    InvalidEscapeSequence { error: String },
 }
 
 impl Error for ScannerError {}
-
-impl ScannerError {
-    fn error_display(&self) -> ErrorDisplay {
-        ErrorDisplay
-    }
-}
 
 impl Display for ScannerError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -66,18 +33,6 @@ impl Display for ScannerError {
             Self::InvalidExponent { error } => write!(f, "Invalid exponent {}", error),
             Self::InvalidNumber { error } => write!(f, "Invalid number {}", error),
             Self::InvalidEscapeSequence { error } => write!(f, "Invalid escape sequence {}", error),
-            Self::InvalidUnicodeSequence {
-                source,
-                start,
-                column_start,
-                line,
-            } => {
-                let ed = self.error_display();
-
-                ed.preview(source.as_str(), *start, *column_start, *line);
-
-                write!(f, "{}", "")
-            }
         }
     }
 }
