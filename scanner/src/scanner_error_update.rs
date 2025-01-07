2284d751edfbf1bba361528be534c627eaf56fd6
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2025-01-07 23:17:02 +0000

    Added error types from scanner error

--- scanner/src/scanner_error_update.rs
+++ scanner/src/scanner_error_update.rs
@@ -29,14 +29,30 @@ impl<'source> Preview<'source> {
 
 #[derive(Debug, PartialEq)]
 pub enum ErrorType {
+    EmptySource,
+    UnknownCharacter,
+    UnknownLiteral,
     UnterminatedString,
+    UnterminatedFractionalNumber,
+    LeadingZeros,
+    InvalidExponent,
+    InvalidNumber,
+    InvalidEscapeSequence,
     InvalidUnicodeSequence,
 }
 
 impl Display for ErrorType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
+            Self::EmptySource => write!(f, "Empty source"),
+            Self::UnknownCharacter => write!(f, "Unknown character"),
+            Self::UnknownLiteral => write!(f, "Unknown literal"),
             Self::UnterminatedString => write!(f, "Unterminated string"),
+            Self::UnterminatedFractionalNumber => write!(f, "Unterminated fractional number"),
+            Self::LeadingZeros => write!(f, "Leading zeros"),
+            Self::InvalidExponent => write!(f, "Invalid exponent"),
+            Self::InvalidNumber => write!(f, "Invalid number"),
+            Self::InvalidEscapeSequence => write!(f, "Invalid escape sequence"),
             Self::InvalidUnicodeSequence => write!(f, "Invalid unicode sequence"),
         }
     }
