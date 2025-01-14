9dab1879088f6ea35678ef02cd80018479e571a0
Alexander Watts <alexanderwatts@Alexanders-MacBook-Pro.local>
2025-01-14 14:10:25 +0000

    Move integration to lib

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,1 +1,31 @@
 //! Integration tests
+
+use format::formatter::Formatter;
+use parser::parser::Parser;
+use scanner::scanner::Scanner;
+
+#[test]
+fn scan_parse_format() {
+    let source = "[]";
+
+    let mut scanner = Scanner::new(source);
+    let tokens = match scanner.scan() {
+        Ok(tokens) => tokens,
+        Err(error) => {
+            return eprintln!("{}", error);
+        }
+    };
+
+    let parser = Parser::new(source, tokens);
+    let ast = match parser.parse() {
+        Ok(ast) => ast,
+        Err(error) => {
+            return eprintln!("{}", error);
+        }
+    };
+
+    let formatter = Formatter::default();
+    let json = formatter.format(&ast);
+
+    assert_eq!("[]", json);
+}
