762d14c32b540301020e4956bf9a8f370e83a913
Alexander Watts <acwatts.dev@gmail.com>
2025-02-27 12:59:43 +0000

    Created token buffer to get and peek tokens without requiring &mut self in parser

---
+++ scanner/src/token_buffer.rs
@@ -0,0 +1,59 @@
+use crate::{scanner::Scanner, scanner_error::ScannerError};
+use std::cell::{Ref, RefCell};
+use token::token::Token;
+
+#[derive(Debug)]
+pub struct TokenBuffer<'source> {
+    pub scanner: Scanner<'source>,
+    pub current_token: RefCell<Result<Token, ScannerError>>,
+}
+
+impl<'source> TokenBuffer<'source> {
+    pub fn new(scanner: Scanner<'source>) -> Self {
+        let initial_token = scanner.get_token();
+
+        Self {
+            scanner,
+            current_token: RefCell::new(initial_token),
+        }
+    }
+
+    pub fn get_token(&self) -> Result<Token, ScannerError> {
+        self.current_token
+            .replace_with(|_| self.scanner.get_token())
+    }
+
+    pub fn peek_token(&self) -> Ref<'_, Result<Token, ScannerError>> {
+        self.current_token.borrow()
+    }
+}
+
+#[cfg(test)]
+mod token_buffer_tests {
+    use token::token_type::TokenType;
+
+    use super::*;
+
+    #[test]
+    pub fn token_buffer_get_and_peek() {
+        let token_buffer = TokenBuffer::new(Scanner::new("{}"));
+
+        assert_eq!(
+            TokenType::LeftBrace,
+            token_buffer.get_token().unwrap().token_type
+        );
+        assert_eq!(
+            TokenType::RightBrace,
+            token_buffer.peek_token().as_ref().unwrap().token_type
+        );
+        assert_eq!(
+            TokenType::RightBrace,
+            token_buffer.peek_token().as_ref().unwrap().token_type
+        );
+        assert_eq!(
+            TokenType::RightBrace,
+            token_buffer.get_token().unwrap().token_type
+        );
+        assert_eq!(TokenType::Eof, token_buffer.get_token().unwrap().token_type);
+    }
+}
