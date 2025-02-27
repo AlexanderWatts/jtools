e1d7e36681ffcb33a38f54c39a11578b2a6393e3
Alexander Watts <acwatts.dev@gmail.com>
2025-02-27 22:52:30 +0000

    Convert scanner errors into strings

--- parser/src/parser_error.rs
+++ parser/src/parser_error.rs
@@ -27,8 +27,8 @@ impl From<ScannerError> for ParserError {
 impl Display for ParserError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            ParserError::ScannerError(_) => {
-                write!(f, "Scanner error")
+            ParserError::ScannerError(scanner_error) => {
+                write!(f, "{}", scanner_error.to_string())
             }
             ParserError::DuplicateProperty {
                 property,
